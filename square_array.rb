c117ced3e784c2e5682ed4c96966be7ebb4e2f8a
Doak Covington <doakcovington@protonmail.com>
2019-12-29 23:30:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,12 @@
 def square_array(array)
   # your code here
+  #empty array
   new_array = []
-
+  #iterate over array
   array.each do |squared|
+    #for each iteration square the current element and add it to new_array
     new_array << squared ** 2
   end
+  #return new_array
   new_array
 end
